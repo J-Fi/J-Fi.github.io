bb9cb6a47154824aa1e59845050996302385dc83
J-Fi <y.neczek@gmail.com>
2019-11-13 01:13:45 +0100

    Change apiRoot and trelloApiRoot to locahost

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
 
-  const apiRoot = "https://afternoon-beyond-29045.herokuapp.com/v1/task/";
-  const trelloApiRoot = 'https://afternoon-beyond-29045.herokuapp.com/v1/trello/';
+  //const apiRoot = "https://afternoon-beyond-29045.herokuapp.com/v1/task/";
+  //const trelloApiRoot = 'https://afternoon-beyond-29045.herokuapp.com/v1/trello/';
+  const apiRoot = "http://localhost:8080/v1/task/";
+  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
